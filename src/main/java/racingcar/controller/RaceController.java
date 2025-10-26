47a95d427a9176af492abf51b79fd78e3e03093b
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-10-26 22:28:37 +0900

    feat($raceController) : 경주 기능 추가
    컨트롤러를 통해서 경주를 진행하고 결과를 출력하는 기능 추가

--- src/main/java/racingcar/controller/RaceController.java
+++ src/main/java/racingcar/controller/RaceController.java
@@ -1,2 +1,19 @@
-package racingcar.controller;public class RaceController {
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.Cars;
+import racingcar.domain.Race;
+import racingcar.view.OutputView;
+
+public class RaceController {
+    public void run(int tryNumber, Cars cars){
+        Race race = new Race(cars);
+        while(tryNumber > 0){
+            race.raceOneRound();
+            OutputView.printProgress(cars);
+            tryNumber--;
+        }
+        List<String> winnerNames =  race.findWinner();
+        OutputView.printWinner(winnerNames);
+    }
 }
