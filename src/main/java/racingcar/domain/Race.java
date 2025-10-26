f521fa5f2319630d9dbd83ce33fe9f082578a24a
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-10-26 22:24:26 +0900

    feat($race) : 우승자 찾는 기능 추가

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,2 +1,32 @@
-package racingcar.domain;public class Race {
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    private Cars cars;
+
+    public Race(Cars cars) {
+        this.cars = cars;
+    }
+
+    public void raceOneRound() {
+        cars.moveAll();
+    }
+
+    public List<String> findWinner() {
+        List<Car> carList = cars.getCars();
+        int maxDistance = carList.stream()
+                .mapToInt(Car::getDistance)
+                .max()
+                .orElse(0);
+
+        List<String> winnerNames = new ArrayList<>();
+        for (Car car : carList) {
+            if (car.getDistance() == maxDistance) {
+                winnerNames.add(car.getName());
+            }
+        }
+        return winnerNames;
+    }
 }
